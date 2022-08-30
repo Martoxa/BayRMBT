edb3637e3ba12c89cc4fdc3a4779d24219e118f1
Pablo Martínez Sosa <martoza@gmail.com>
2022-08-29 17:05:59 -0700

    Update bayRmbt_model.R
    Completely translated version of bayMBT_model. I need to comment the script.

--- bayRmbt_model.R
+++ bayRmbt_model.R
@@ -2,6 +2,7 @@ bayrmbt_model<-function(Tmodel,Type){
 
   if(!require(MASS)){cat("informative error message")}
   if(!require(Rlab)){cat("informative error message")}
+  if(!require(ks)){cat("informative error message")}
 ##Select type of sample
       if(Type == "soil"){
     calibs<-read.csv(file("soil_calibration_data.csv"),head=TRUE,sep=',')
@@ -78,5 +79,53 @@ bayrmbt_model<-function(Tmodel,Type){
   Ws_var<-mean(apply(b_draws[,,1],1,var))
   Rhat<-sqrt(((n-1)/n*Ws_var+1/n*Bs_var)/Ws_var) #Rhat should be close to 1 if convergence is achieved.
   
+##Plot posteriors with priors
   
+  #Slope
+  slope<-b_draws[,,1]
+  xts<-seq(min(slope)-0.01,max(slope)+0.01,by=0.0005)
+  posts<-kde(c(slope),eval.points = xts) #x<-post$eval, y<-post$estimate
+  priors<-dnorm(xts,mean = m_p[1],sd=sqrt(mean(cov_p[1,])))
+  
+  #Intercept
+  intercept<-b_draws[,,2]
+  xti<-seq(min(intercept)-0.01,max(intercept)+0.01,by=0.0005)
+  posti<-kde(c(intercept),eval.points = xti)
+  priori<-dnorm(xti,mean = m_p[1],sd=sqrt(mean(cov_p[1,])))
+  
+  #Error variance
+  xte<-seq(0,0.02,by=0.0001)
+  poste<-kde(c(tau2_draws),eval.points = xte)
+  priore<-b^a/gamma(a)*xte^(-a-1)*exp(-b/xte)
+  
+  #Plot
+  par(mfrow=c(1,3))
+  plot(xts,posts$estimate,type='l',col="red",lwd=2, main = "Slope",ylab = "Prob. density",xlab="")
+  lines(xts,priors,col="black",lwd=2)
+  legend("topleft",legend = c("Prior","Posterior"),col = c("black","red"),lty=1,lwd = 2)
+  
+  plot(xti,posti$estimate,type='l',col="red",lwd=2, main = "Intercept",ylab="",xlab="")
+  lines(xti,priori,col="black",lwd=2)
+  legend("topleft",legend = c("Prior","Posterior"),col = c("black","red"),lty=1,lwd = 2)
+  
+  plot(xte,poste$estimate,type='l',col="red",lwd=2, main = "Error variance (t^2)",ylab="",xlab="")
+  lines(xte,priore,col="black",lwd=2)
+  legend("topleft",legend = c("Prior","Posterior"),col = c("black","red"),lty=1,lwd = 2)
+  
+##Reshape and save
+  b_draws_r<-matrix(b_draws,nrow=dim(b_draws)[1]*dim(b_draws)[2],ncol=dim(b_draws)[3])
+  tau2_draws_r<-matrix(tau2_draws,dim(tau2_draws)[1]*dim(tau2_draws)[2],1)
+  b_draws_final<-b_draws_r[seq(1,dim(b_draws_r)[1],by=4),]
+  tau2_draws_final<-tau2_draws_r[seq(1,dim(tau2_draws_r)[1],by=4),]
+  param_final<-list(b_draws_final,tau2_draws_final)
+  
+  if(Type == "soil" & Tmodel == "T") {
+    saveRDS(param_final,file = "baymbt_params_soil.RDS")
+  } else if(Type == "soil" & Tmodel == "T0"){
+    saveRDS(param_final,file = "baymbt0_params_soil.RDS")
+  } else if(Type == "lake" & Tmodel == "T"){
+    saveRDS(param_final,file = "baymbt_params_lake.RDS")
+  } else if(Type == "lake" & Tmodel == "T0"){
+    saveRDS(param_final,file = "baymbt0_params_lake.RDS")
+  }
 }  
